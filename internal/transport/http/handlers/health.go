aa4a4d242f3651dcc586f58d766eaf82fb10a85d
Sergey <dyuzhovsergey@gmail.com>
2026-04-09 14:05:01 +0300

    added main: init logger, create router, start server

--- internal/transport/http/handlers/health.go
+++ internal/transport/http/handlers/health.go
@@ -15,5 +15,5 @@ func Health(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 
-	_, _ = w.Write([]byte("ok"))
+	_, _ = w.Write([]byte("ok\n"))
 }
