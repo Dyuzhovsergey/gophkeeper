7a07494ac2064d8d36d9417829bbcc67ca1c21b5
Sergey <dyuzhovsergey@gmail.com>
2026-04-15 21:27:41 +0300

    added query for secrets

--- internal/repository/postgres/postgres.go
+++ internal/repository/postgres/postgres.go
@@ -17,6 +17,11 @@ type SessionsRepository struct {
 	db *sql.DB
 }
 
+// SecretsRepository реализует SecretRepository поверх PostgreSQL.
+type SecretsRepository struct {
+	db *sql.DB
+}
+
 // NewUsersRepository создаёт репозиторий пользователей.
 func NewUsersRepository(db *sql.DB) *UsersRepository {
 	return &UsersRepository{db: db}
@@ -27,5 +32,11 @@ func NewSessionsRepository(db *sql.DB) *SessionsRepository {
 	return &SessionsRepository{db: db}
 }
 
+// NewSecretsRepository создаёт репозиторий секретов.
+func NewSecretsRepository(db *sql.DB) *SecretsRepository {
+	return &SecretsRepository{db: db}
+}
+
 var _ repo.UserRepository = (*UsersRepository)(nil)
 var _ repo.SessionRepository = (*SessionsRepository)(nil)
+var _ repo.SecretRepository = (*SecretsRepository)(nil)
